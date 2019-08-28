fe0fdd0f7848725108bb1ecd0233dd5236f096bc
ganhouqi <1272404715@qq.com>
2019-08-28 10:50:02 +0800

    使用DI依赖注入

--- src/com/apress/prospring5/ch2/HelloWorldSpringDI.java
+++ src/com/apress/prospring5/ch2/HelloWorldSpringDI.java
@@ -1,4 +1,14 @@
-package com.apress.prospring5.ch2.decoupled;
+package com.apress.prospring5.ch2;
+
+import com.apress.prospring5.ch2.decoupled.MessageRenderer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class HelloWorldSpringDI {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext= new ClassPathXmlApplicationContext("app-context.xml");
+        MessageRenderer messageRenderer=applicationContext.getBean("renderer",MessageRenderer.class);
+        messageRenderer.render();
+    }
 }
+
