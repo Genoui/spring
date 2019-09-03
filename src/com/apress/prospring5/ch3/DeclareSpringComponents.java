8cd730ab545116835bebe68f89041ffbca987cd4
ganhouqi <1272404715@qq.com>
2019-09-03 16:14:55 +0800

    update

--- src/com/apress/prospring5/ch3/DeclareSpringComponents.java
+++ src/com/apress/prospring5/ch3/DeclareSpringComponents.java
@@ -1,16 +1,15 @@
 package com.apress.prospring5.ch3;
 
-import com.apress.prospring5.ch2.decoupled.MessageRenderer;
+import com.apress.prospring5.ch2.decoupled.MessageProvider;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class DeclareSpringComponents {
     public static void main(String[] args) {
         GenericXmlApplicationContext ctx=new GenericXmlApplicationContext();
-       // ctx.load("classpath:app-context-annotation.xml");
         ctx.load("classpath:app-context-xml.xml");
         ctx.refresh();
-        MessageRenderer messageRenderer=ctx.getBean("renderer",MessageRenderer.class);
-        messageRenderer.render();
-        ctx.close();
+        MessageProvider messageProvider=ctx.getBean("messageProvider", MessageProvider.class);
+        System.out.println(messageProvider.getMessage());
     }
+
 }
--- src/com/apress/prospring5/ch3/annotation/ConfigurableMessageProvider.java
+++ src/com/apress/prospring5/ch3/annotation/ConfigurableMessageProvider.java
@@ -2,6 +2,7 @@ package com.apress.prospring5.ch3.annotation;
 
 import com.apress.prospring5.ch2.decoupled.MessageProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service("provider")
@@ -15,6 +16,6 @@ public class ConfigurableMessageProvider implements MessageProvider {
 
     @Override
     public String getMessage() {
-        return null;
+        return this.message;
     }
 }
